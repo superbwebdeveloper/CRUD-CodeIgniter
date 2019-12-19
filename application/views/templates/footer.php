d32f0fe5043b25b2f29b41cd7bc167a97dd1a3eb
superbwebdeveloper <superbwebdeveloper@users.noreply.github.com>
2019-12-19 15:12:16 +0530

    Create footer.php

---
+++ application/views/templates/footer.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <div class="footer_logo"><img src="<?php echo base_url(); ?>resource/images/footer_logo.png" alt="" /></div>
+                <p class="center_text">Proin mattis rutrum magna, et pretium erat tincidunt at.<br />Donec lectus ipsum, pulvinar id sollicitudin id, bibendum vel quam.</p>
+
+                <div class="col-md-4 col-sm-4 col-xs-12 text_align_right">Wakefeild, RI 02879<br />682, Kingstown Road</div>
+                <div class="col-md-4 col-sm-4 col-xs-12 social_icon">
+                    <ul>
+                        <li><a href="#"><img src="<?php echo base_url(); ?>resource/images/facebook_icon.png" alt="" /></a></li>
+                        <li><a href="#"><img src="<?php echo base_url(); ?>resource/images/twitter_icon.png" alt="" /></a></li>
+                        <li><a href="#"><img src="<?php echo base_url(); ?>resource/images/google_pluse_icon.png" alt="" /></a></li>
+                        <li><a href="#"><img src="<?php echo base_url(); ?>resource/images/pinrest_icon.png" alt="" /></a></li>
+                        <li><a href="#"><img src="<?php echo base_url(); ?>resource/images/linkedin_icon.png" alt="" /></a></li>
+
+                    </ul>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12 text_align_left">Phone: (924)-782-8008</div>
+
+                <div class="copyright">© cey cey’s cafe&Bakery 2018. All rights reserved.</div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+
+
+
+
+<script type="text/javascript" src="<?php echo base_url(); ?>resource/js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>resource/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>resource/js/simpleMobileMenu.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>resource/js/custom.js"></script>
+</body>
+
+</html>
