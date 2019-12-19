989f6981cb49f3b1c688051f6bfff31ed8ae690c
superbwebdeveloper <superbwebdeveloper@users.noreply.github.com>
2019-12-19 15:10:18 +0530

    Create home_page.php

---
+++ application/views/home_page.php
@@ -0,0 +1,308 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<section class="header_slider">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="<?php echo base_url(); ?>resource/images/slider1.jpg" alt="" />
+                <div class="carousel-caption">
+                    <h1>A Delicious Range of Unique Bakery Fare<br />and Hand Crafted Sandwiches</h1>
+                    <a href="#" class="btn btn-default btn-lg" role="button">Learn More</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="<?php echo base_url(); ?>resource/images/slider1.jpg" alt="" />
+                <div class="carousel-caption">
+                    <h1>A Delicious Range of Unique Bakery Fare<br />and Hand Crafted Sandwiches</h1>
+                    <a href="#" class="btn btn-default btn-lg" role="button">Learn More</a>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="<?php echo base_url(); ?>resource/images/slider1.jpg" alt="" />
+                <div class="carousel-caption">
+                    <h1>A Delicious Range of Unique Bakery Fare<br />and Hand Crafted Sandwiches</h1>
+                    <a href="#" class="btn btn-default btn-lg" role="button">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+
+<section class="services">
+
+    <div class="container">
+
+        <h1><span>About</span>
+            Service & Deliveries
+            <span class="seprator"><img src="<?php echo base_url(); ?>resource/images/heading-seprator.png" alt="" /></span>
+        </h1>
+
+        <div class="row">
+
+            <div class="service_card col-lg-3 col-md-3 col-sm-3 col-xs-12">
+
+                <img src="<?php echo base_url(); ?>resource/images/order_online.png" alt="" />
+                <h2>Order Online</h2>
+                <p>Suspendisse potenti. Etiam non euismod augue.</p>
+            </div>
+
+            <div class="service_card col-lg-3 col-md-3 col-sm-3 col-xs-12">
+
+                <img src="<?php echo base_url(); ?>resource/images/customagable_gift.png" alt="" />
+                <h2>Customizable gift</h2>
+                <p>Suspendisse potenti. Etiam non euismod augue.</p>
+            </div>
+
+            <div class="service_card col-lg-3 col-md-3 col-sm-3 col-xs-12">
+
+                <img src="<?php echo base_url(); ?>resource/images/free_delivery.png" alt="" />
+                <h2>Free Delivery</h2>
+                <p>Suspendisse potenti. Etiam non euismod augue.</p>
+            </div>
+
+
+            <div class="service_card col-lg-3 col-md-3 col-sm-3 col-xs-12">
+
+                <img src="<?php echo base_url(); ?>resource/images/safe_payment.png" alt="" />
+                <h2>Safe Payment</h2>
+                <p>Suspendisse potenti. Etiam non euismod augue.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="spread_cheer">
+
+    <div class="container">
+
+        <h1><span>Spread Cheer</span>
+            Always fresh and crafted right perfect just for you
+            <span class="seprator"><img src="<?php echo base_url(); ?>resource/images/heading-seprator.png" alt="" /></span>
+        </h1>
+
+        <div class="row">
+
+            <div class="spread_card col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <h2>Cakes</h2>
+                <img src="<?php echo base_url(); ?>resource/images/cakes.jpg" alt="" />
+                <p> Suspendisse potenti. Etiam non euismod augue. Nam aliquam sit amet tortor sit amet consectetur.</p>
+            </div>
+
+            <div class="spread_card col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <h2>Salads</h2>
+                <img src="<?php echo base_url(); ?>resource/images/salads.jpg" alt="" />
+                <p> Suspendisse potenti. Etiam non euismod augue. Nam aliquam sit amet tortor sit amet consectetur.</p>
+            </div>
+
+            <div class="spread_card col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <h2>Bakery</h2>
+                <img src="<?php echo base_url(); ?>resource/images/bakery.jpg" alt="" />
+                <p> Suspendisse potenti. Etiam non euismod augue. Nam aliquam sit amet tortor sit amet consectetur.</p>
+            </div>
+
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="cafe_bakery">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="cake_card col-lg-6 col-md-6 col-sm-6"><img src="<?php echo base_url(); ?>resource/images/cake.png" alt="" /></div>
+
+            <div class="cake_card_2 col-lg-6 col-md-6 col-sm-6">
+
+                <h1><span>About us</span>
+                    Cey Cey’s Cafe & Bakery
+                    <span class="seprator"><img src="<?php echo base_url(); ?>resource/images/heading-seprator.png" alt="" /></span>
+                </h1>
+                <p>Lorem ipsum dolor sit amet, sit pellentesque pellentesque ultrices,
+                    et egestas eu, nascetur justo, urna rutrum dignissim malesuada, purus
+                    risus. Odio vulputate. Eget sapiente sit, nibh eget odio iaculis risus.
+                    Ultricies urna tempor diam, amet magnis tellus in ante pharetra
+                    maecenas, dolor pede est. Purus volutpat, aliquam consequat maecenas
+                    ultrices nibh sit id, vestibulum ipsum phasellus amet eget.</p>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+
+<section class="discover_more">
+
+    <div class="container">
+
+        <h1><span>Discover More Of</span>
+            Our Sweet Selection
+            <span class="seprator"><img src="<?php echo base_url(); ?>resource/images/heading-seprator.png" alt="" /></span>
+        </h1>
+
+        <div class="row">
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Sandwich</h2>
+                <img src="<?php echo base_url(); ?>resource/images/sanwitch.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Brownie</h2>
+                <img src="<?php echo base_url(); ?>resource/images/brownie.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Cup cake</h2>
+                <img src="<?php echo base_url(); ?>resource/images/cup-cake.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Pound Cakes</h2>
+                <img src="<?php echo base_url(); ?>resource/images/pound-cake.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Donuts</h2>
+                <img src="<?php echo base_url(); ?>resource/images/donutes.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+            <div class="discover_card col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                <h2>Waffles</h2>
+                <img src="<?php echo base_url(); ?>resource/images/waffles.jpg" alt="" />
+                <p>Suspendisse potenti. Etiam non euismod augue. </p>
+                <h3>From $25.50</h3>
+            </div>
+
+
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="testimonials">
+
+    <div class="container">
+
+        <h1>
+            <span>Clients sayings</span>
+            Testimonials
+            <span class="seprator"><img src="<?php echo base_url(); ?>resource/images/heading-seprator.png" alt="" /></span>
+        </h1>
+
+
+        <div class="row">
+
+            <div class="test_box col-lg-6 col-md-6 col-sm-6">
+
+                <div class="image_box col-lg-6">
+                    <img src="<?php echo base_url(); ?>resource/images/test-1.jpg" alt="" />
+                </div>
+                <div class="col-lg-6">
+                    <h2>Candice patron</h2>
+                    <div class="time_date"><label><span><img src="<?php echo base_url(); ?>resource/images/watch_icon.png" alt="" /></span> Jan 16,2018</label> <label><span><img src="<?php echo base_url(); ?>resource/images/user_icon.png" alt="" /></span> Foodie</label> </div>
+                    <p> Maecenas fermentum tellus nisi, eget ullamcorper nibh porttitor nec. Duis ut faucibus.</p>
+
+                </div>
+
+            </div>
+
+            <div class="test_box col-lg-6 col-md-6 col-sm-6">
+
+                <div class="image_box col-lg-6">
+                    <img src="<?php echo base_url(); ?>resource/images/test-2.jpg" alt="" />
+                </div>
+                <div class="col-lg-6">
+                    <h2>Morgan kelly</h2>
+                    <div class="time_date"><label><span><img src="<?php echo base_url(); ?>resource/images/watch_icon.png" alt="" /></span> Feb 04,2018</label> <label><span><img src="<?php echo base_url(); ?>resource/images/user_icon.png" alt="" /></span> Blogger</label> </div>
+                    <p> Maecenas fermentum tellus nisi, eget ullamcorper nibh porttitor nec. Duis ut faucibus.</p>
+
+                </div>
+
+            </div>
+
+
+            <div class="test_box col-lg-6 col-md-6 col-sm-6">
+
+                <div class="col-lg-6">
+                    <h2>Harvey Zester</h2>
+                    <div class="time_date"><label><span><img src="<?php echo base_url(); ?>resource/images/watch_icon.png" alt="" /></span> Jan 16,2018</label> <label><span><img src="<?php echo base_url(); ?>resource/images/user_icon.png" alt="" /></span> Nutritionist</label> </div>
+                    <p> Maecenas fermentum tellus nisi, eget ullamcorper nibh porttitor nec. Duis ut faucibus.</p>
+
+                </div>
+
+                <div class="image_box col-lg-6">
+                    <img src="<?php echo base_url(); ?>resource/images/test-3.jpg" alt="" />
+                </div>
+
+            </div>
+
+            <div class="test_box col-lg-6 col-md-6 col-sm-6">
+
+                <div class="col-lg-6">
+                    <h2>Janice Gellar</h2>
+                    <div class="time_date"><label><span><img src="<?php echo base_url(); ?>resource/images/watch_icon.png" alt="" /></span> Jan 16,2018</label> <label><span><img src="<?php echo base_url(); ?>resource/images/user_icon.png" alt="" /></span> Journalist</label> </div>
+                    <p> Maecenas fermentum tellus nisi, eget ullamcorper nibh porttitor nec. Duis ut faucibus.</p>
+
+                </div>
+
+                <div class="image_box col-lg-6">
+                    <img src="<?php echo base_url(); ?>resource/images/test-4.jpg" alt="" />
+                </div>
+
+            </div>
+
+            <div class="view_btn col-lg-12">
+                <a href="#" class="btn btn-default btn-lg" role="button">View All</a>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</section>
